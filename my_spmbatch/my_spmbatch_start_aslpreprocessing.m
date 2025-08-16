2a3055702484f3ca5efc58c7d200dc1ac5f55705
dr. Van Schuerbeek P. <60138268+P-VS@users.noreply.github.com>
2025-08-16 10:47:22 +0200

    Add files via upload

--- my_spmbatch/my_spmbatch_start_aslpreprocessing.m
+++ my_spmbatch/my_spmbatch_start_aslpreprocessing.m
@@ -6,108 +6,108 @@ save(fullfile(datpath,'params.mat'),'params')
 
 datlist = zeros(numel(sublist)*numel(nsessions)*numel(params.asl.runs),3);
 
-dpos = 1;
-for i = 1:numel(sublist)
-    for j = 1:numel(nsessions)
-        for k = 1:numel(params.asl.runs)
-            datlist(dpos,1) = sublist(i);
-            datlist(dpos,2) = nsessions(j);
-            datlist(dpos,3) = params.asl.runs(k);
-    
-            dpos = dpos+1;
-        end
-    end
-end
-
-numpacks = ceil(numel(datlist(:,1))/params.maxprocesses);
-
-if params.use_parallel
-    for j=1:numpacks
-        if (j*params.maxprocesses)<=numel(datlist(:,1))
-            maxruns = params.maxprocesses;
-        else
-            maxruns = params.maxprocesses-((j*params.maxprocesses)-numel(datlist(:,1)));
-        end
-
-        for is = 1:maxruns
-            i = (j-1)*params.maxprocesses+is;
-
-            t = datetime('now','TimeZone','local','Format','d-MMM-y HH:mm:ss');
-            fprintf(['\n'  datestr(t) ' : Start preprocessing data for subject ' num2str(datlist(i,1)) ' session ' num2str(datlist(i,2)) ' run ' num2str(datlist(i,3)) '\n'])
-    
-            logfile{i} = fullfile(datpath,['asl_preprocess_logfile_' sprintf('%02d',datlist(i,1)) '_' sprintf('%02d',datlist(i,2)) '_' sprintf('%02d',datlist(i,3)) '.txt']);
-    
-            if exist(logfile{i},'file'), delete(logfile{i}); end
-            
-            if ispc
-                mtlb_cmd = sprintf("restoredefaultpath;addpath(genpath('%s'));addpath(genpath('%s'));my_spmbatch_run_aslpreprocessing(%d,%d,%d,'%s','%s');exit", ...
-                                        params.spm_path,params.my_spmbatch_path,datlist(i,1),datlist(i,2),datlist(i,3),datpath,fullfile(datpath,'params.mat'));
-
-                system_cmd = sprintf(['start matlab -nodesktop -nosplash -r "%s" -logfile %s'],mtlb_cmd,logfile{i});
-            else
-                mtlb_cmd = sprintf('"restoredefaultpath;addpath(genpath(''%s''));addpath(genpath(''%s''));my_spmbatch_run_aslpreprocessing(%d,%d,%d,''%s'',''%s'');exit"', ...
-                                        params.spm_path,params.my_spmbatch_path,datlist(i,1),datlist(i,2),datlist(i,3),datpath,fullfile(datpath,'params.mat'));
-
-                system_cmd = sprintf([fullfile(matlabroot,'bin') '/matlab -nosplash -r ' mtlb_cmd ' -logfile ' logfile{i} ' & ']);
+for k = 1:numel(params.asl.runs)
+    dpos = 1;
+    for i = 1:numel(sublist)
+        for j = 1:numel(nsessions)
+                datlist(dpos,1) = sublist(i);
+                datlist(dpos,2) = nsessions(j);
+                datlist(dpos,3) = params.asl.runs(k);
+        
+                dpos = dpos+1;
             end
-            [status,result]=system(system_cmd);
-        end
+    end
     
-        %% wait for all processing to be finnished
-        isrunning = true;
-        pfinnished = 0;
-        while isrunning
+    numpacks = ceil(numel(datlist(:,1))/params.maxprocesses);
+    
+    if params.use_parallel
+        for j=1:numpacks
+            if (j*params.maxprocesses)<=numel(datlist(:,1))
+                maxruns = params.maxprocesses;
+            else
+                maxruns = params.maxprocesses-((j*params.maxprocesses)-numel(datlist(:,1)));
+            end
+    
             for is = 1:maxruns
                 i = (j-1)*params.maxprocesses+is;
-
-                if exist(logfile{i},'file')
-                    FID     = fopen(logfile{i},'r');
-                    txt     = textscan(FID,'%s');
-                    txt     = txt{1}; 
-                    test=find(cellfun('isempty',strfind(txt,'PP_Completed'))==0,1,'first');
-                    errortest=find(cellfun('isempty',strfind(txt,'PP_Error'))==0,1,'first');
-                    fclose(FID);
-
-                    if ~isempty(errortest)
-                        pfinnished = pfinnished+1;
-
-                        nlogfname = fullfile(datpath,['error_asl_preprocess_logfile_' sprintf('%02d',datlist(i,1)) '_' sprintf('%02d',datlist(i,2)) '_' sprintf('%02d',datlist(i,3)) '.txt']);
-                        movefile(logfile{i},nlogfname);
-
-                        t = datetime('now','TimeZone','local','Format','d-MMM-y HH:mm:ss');
-                        fprintf(['\n'  datestr(t) ' : Error during preprocessing data for subject ' num2str(datlist(i,1)) ' session ' num2str(datlist(i,2)) ' run ' num2str(datlist(i,3)) '\n'])
-                    elseif ~isempty(test)
-                        pfinnished = pfinnished+1;
-
-                        if ~params.keeplogs
-                            delete(logfile{i}); 
-                        else
-                            nlogfname = fullfile(datpath,['done_asl_preprocess_logfile_' sprintf('%02d',datlist(i,1)) '_' sprintf('%02d',datlist(i,2)) '_' sprintf('%02d',datlist(i,3)) '.txt']);
+    
+                t = datetime('now','TimeZone','local','Format','d-MMM-y HH:mm:ss');
+                fprintf(['\n'  datestr(t) ' : Start preprocessing data for subject ' num2str(datlist(i,1)) ' session ' num2str(datlist(i,2)) ' run ' num2str(datlist(i,3)) '\n'])
+        
+                logfile{i} = fullfile(datpath,['asl_preprocess_logfile_' sprintf('%02d',datlist(i,1)) '_' sprintf('%02d',datlist(i,2)) '_' sprintf('%02d',datlist(i,3)) '.txt']);
+        
+                if exist(logfile{i},'file'), delete(logfile{i}); end
+                
+                if ispc
+                    mtlb_cmd = sprintf("restoredefaultpath;addpath(genpath('%s'));addpath(genpath('%s'));my_spmbatch_run_aslpreprocessing(%d,%d,%d,'%s','%s');exit", ...
+                                            params.spm_path,params.my_spmbatch_path,datlist(i,1),datlist(i,2),datlist(i,3),datpath,fullfile(datpath,'params.mat'));
+    
+                    system_cmd = sprintf(['start matlab -nodesktop -nosplash -r "%s" -logfile %s'],mtlb_cmd,logfile{i});
+                else
+                    mtlb_cmd = sprintf('"restoredefaultpath;addpath(genpath(''%s''));addpath(genpath(''%s''));my_spmbatch_run_aslpreprocessing(%d,%d,%d,''%s'',''%s'');exit"', ...
+                                            params.spm_path,params.my_spmbatch_path,datlist(i,1),datlist(i,2),datlist(i,3),datpath,fullfile(datpath,'params.mat'));
+    
+                    system_cmd = sprintf([fullfile(matlabroot,'bin') '/matlab -nosplash -r ' mtlb_cmd ' -logfile ' logfile{i} ' & ']);
+                end
+                [status,result]=system(system_cmd);
+            end
+        
+            %% wait for all processing to be finnished
+            isrunning = true;
+            pfinnished = 0;
+            while isrunning
+                for is = 1:maxruns
+                    i = (j-1)*params.maxprocesses+is;
+    
+                    if exist(logfile{i},'file')
+                        FID     = fopen(logfile{i},'r');
+                        txt     = textscan(FID,'%s');
+                        txt     = txt{1}; 
+                        test=find(cellfun('isempty',strfind(txt,'PP_Completed'))==0,1,'first');
+                        errortest=find(cellfun('isempty',strfind(txt,'PP_Error'))==0,1,'first');
+                        fclose(FID);
+    
+                        if ~isempty(errortest)
+                            pfinnished = pfinnished+1;
+    
+                            nlogfname = fullfile(datpath,['error_asl_preprocess_logfile_' sprintf('%02d',datlist(i,1)) '_' sprintf('%02d',datlist(i,2)) '_' sprintf('%02d',datlist(i,3)) '.txt']);
                             movefile(logfile{i},nlogfname);
+    
+                            t = datetime('now','TimeZone','local','Format','d-MMM-y HH:mm:ss');
+                            fprintf(['\n'  datestr(t) ' : Error during preprocessing data for subject ' num2str(datlist(i,1)) ' session ' num2str(datlist(i,2)) ' run ' num2str(datlist(i,3)) '\n'])
+                        elseif ~isempty(test)
+                            pfinnished = pfinnished+1;
+    
+                            if ~params.keeplogs
+                                delete(logfile{i}); 
+                            else
+                                nlogfname = fullfile(datpath,['done_asl_preprocess_logfile_' sprintf('%02d',datlist(i,1)) '_' sprintf('%02d',datlist(i,2)) '_' sprintf('%02d',datlist(i,3)) '.txt']);
+                                movefile(logfile{i},nlogfname);
+                            end
+    
+                            t = datetime('now','TimeZone','local','Format','d-MMM-y HH:mm:ss');
+                            fprintf(['\n'  datestr(t) ' : Done preprocessing data for subject ' num2str(datlist(i,1)) ' session ' num2str(datlist(i,2)) ' run ' num2str(datlist(i,3)) '\n'])
                         end
-
-                        t = datetime('now','TimeZone','local','Format','d-MMM-y HH:mm:ss');
-                        fprintf(['\n'  datestr(t) ' : Done preprocessing data for subject ' num2str(datlist(i,1)) ' session ' num2str(datlist(i,2)) ' run ' num2str(datlist(i,3)) '\n'])
                     end
                 end
-            end
-    
-            if pfinnished>=maxruns 
-                isrunning = false; 
-            else
-                pause(60);
+        
+                if pfinnished>=maxruns 
+                    isrunning = false; 
+                else
+                    pause(60);
+                end
             end
         end
-    end
-else
-    for i=1:numel(datlist(:,1))
-        itstart = tic;
-
-        my_spmbatch_run_aslpreprocessing(datlist(i,1),datlist(i,2),datlist(i,3),datpath,fullfile(datpath,'params.mat'));
-
-        itstop = toc(itstart);
-
-        fprintf(['subject ' num2str(datlist(i,1)) ' session ' num2str(datlist(i,2)) ' run ' num2str(datlist(i,3)) ' processed in ' datestr(duration([0,0,itstop],'InputFormat','ss'),'HH:MM:SS') '\n'])
+    else
+        for i=1:numel(datlist(:,1))
+            itstart = tic;
+    
+            my_spmbatch_run_aslpreprocessing(datlist(i,1),datlist(i,2),datlist(i,3),datpath,fullfile(datpath,'params.mat'));
+    
+            itstop = toc(itstart);
+    
+            fprintf(['subject ' num2str(datlist(i,1)) ' session ' num2str(datlist(i,2)) ' run ' num2str(datlist(i,3)) ' processed in ' datestr(duration([0,0,itstop],'InputFormat','ss'),'HH:MM:SS') '\n'])
+        end
     end
 end
 
