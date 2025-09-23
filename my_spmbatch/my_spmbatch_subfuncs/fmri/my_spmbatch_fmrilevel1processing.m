7b5d1de348bc352efda95bb0cace469a7760df11
dr. Van Schuerbeek P. <60138268+P-VS@users.noreply.github.com>
2025-09-23 11:46:14 +0200

    Add files via upload

--- my_spmbatch/my_spmbatch_subfuncs/fmri/my_spmbatch_fmrilevel1processing.m
+++ my_spmbatch/my_spmbatch_subfuncs/fmri/my_spmbatch_fmrilevel1processing.m
@@ -1,11 +1,11 @@
-function matlabbatch = my_spmbatch_fmrilevel1processing(sub,ses,run,task,datpath,params)
-
-matlabbatch = {};
+function params = my_spmbatch_fmrilevel1processing(sub,ses,run,task,datpath,params)
 
 %% Search for the data folders
+
 ppparams = my_spmbatch_1stlevel_FindData(sub,ses,run,task,datpath,params);
 
 %% fMRI model specification
+
 if params.func.mruns && contains(params.func.use_runs,'separately')
     ppparams.resultmap = fullfile(ppparams.subpath,['SPMMAT-' task '_' params.analysisname '_run-' num2str(run)]);
 else
@@ -15,9 +15,11 @@ end
 if exist(ppparams.resultmap,'dir'); rmdir(ppparams.resultmap,'s'); end
 mkdir(ppparams.resultmap)
 
+matlabbatch = {};
 [matlabbatch,ppparams] = my_spmbatch_1stlevel_DefineModel(sub,ses,run,task,datpath,params,ppparams,matlabbatch);
 
 %% Optimize GLM with TEDM
+
 if params.optimize_HRF
     spm_jobman('run', matlabbatch)
 
@@ -92,43 +94,46 @@ end
 
 matlabbatch{mbidx}.spm.stats.con.delete = 0;
 
-mbidx=mbidx+1;
+spm_jobman('run', matlabbatch);
 
 %% SPM Results
+
 if params.save_spm_results
-    matlabbatch{mbidx}.spm.stats.results.spmmat = cfg_dep('Contrast Manager: SPM.mat File', substruct('.','val', '{}',{mbidx-1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
-    matlabbatch{mbidx}.spm.stats.results.conspec.titlestr = '';
-    matlabbatch{mbidx}.spm.stats.results.conspec.contrasts = Inf;
-    matlabbatch{mbidx}.spm.stats.results.conspec.threshdesc = params.threshold_correction;
-    matlabbatch{mbidx}.spm.stats.results.conspec.thresh = params.pthreshold;
-    matlabbatch{mbidx}.spm.stats.results.conspec.extent = params.kthreshold;
-    matlabbatch{mbidx}.spm.stats.results.conspec.conjunction = 1;
-    matlabbatch{mbidx}.spm.stats.results.conspec.mask.none = 1;
-    matlabbatch{mbidx}.spm.stats.results.units = 1;
+    results.spmmat = {fullfile(ppparams.resultmap,'SPM.mat')};
+    results.conspec.titlestr = '';
+    results.conspec.contrasts = Inf;
+    results.conspec.threshdesc = params.threshold_correction;
+    results.conspec.thresh = params.pthreshold;
+    results.conspec.extent = params.kthreshold;
+    results.conspec.conjunction = 1;
+    results.conspec.mask.none = 1;
+    results.units = 1;
 
     oi = 1;
     if params.save_thresholded_map
-        matlabbatch{mbidx}.spm.stats.results.export{oi}.tspm.basename = 'thres';
+        results.export{oi}.tspm.basename = 'thres';
         oi = oi+1;
     end
     if params.save_binary_mask
-        matlabbatch{mbidx}.spm.stats.results.export{oi}.binary.basename = 'bin';
+        results.export{oi}.binary.basename = 'bin';
         oi = oi+1;
     end
     if params.save_naray
-        matlabbatch{mbidx}.spm.stats.results.export{oi}.nary.basename = 'n-aray';
+        results.export{oi}.nary.basename = 'n-aray';
         oi = oi+1;
     end
     if params.save_csv_file
-        matlabbatch{mbidx}.spm.stats.results.export{oi}.csv = true;
+        results.export{oi}.csv = true;
         oi = oi+1;
     end
     if params.save_pdf_file
-        matlabbatch{mbidx}.spm.stats.results.export{oi}.pdf = true;
+        results.export{oi}.pdf = true;
         oi = oi+1;
     end
     if params.save_tiff_file
-        matlabbatch{mbidx}.spm.stats.results.export{oi}.tif = true;
+        results.export{oi}.tif = true;
         oi = oi+1;
     end
+
+    my_spmbatch_run_results(results);
 end
