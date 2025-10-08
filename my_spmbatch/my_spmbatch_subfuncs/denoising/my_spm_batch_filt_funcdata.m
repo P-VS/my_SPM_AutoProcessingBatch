722ea8deceb65e4fbade850c790c93e40c5a1be6
dr. Van Schuerbeek P. <60138268+P-VS@users.noreply.github.com>
2025-10-08 15:18:34 +0200

    Add files via upload

--- my_spmbatch/my_spmbatch_subfuncs/denoising/my_spm_batch_filt_funcdata.m
+++ my_spmbatch/my_spmbatch_subfuncs/denoising/my_spm_batch_filt_funcdata.m
@@ -11,11 +11,16 @@ s = size(funcdat);
 mean_funcdat = mean(funcdat,2);
 funcdat = funcdat - repmat(mean_funcdat,[1,s(end)]);
 
-if ~(ltrh==-1)
-    bpdat = brant_Filter_FFT_Butterworth(funcdat', -1, ltrh, Fs, 0)';
-    funcdat = funcdat-bpdat;
+nvox = numel(funcdat(:,1));
+for i=1:5000:nvox
+    iend = min(i+5000-1,nvox);
+
+    if ~(ltrh==-1)
+        bpdat = brant_Filter_FFT_Butterworth(funcdat(i:iend,:)', -1, ltrh, Fs, 0)';
+        funcdat(i:iend,:) = funcdat(i:iend,:)-bpdat;
+    end
+    if ~(htrh==-1), funcdat(i:iend,:) = brant_Filter_FFT_Butterworth(funcdat(i:iend,:)', -1, htrh, Fs, 0)'; end
 end
-if ~(htrh==-1), funcdat = brant_Filter_FFT_Butterworth(funcdat', -1, htrh, Fs, 0)'; end
 
 funcdat = funcdat + repmat(mean_funcdat,[1,s(end)]);
 
